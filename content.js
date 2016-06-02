7c71814315a10bd36cef4c7777ce1e2107ad9460
Cameron Eckelberry <cameck@gmail.com>
2016-06-02 17:42:20 -0400

    Added ability to use arrays for target words

--- content.js
+++ content.js
@@ -1,5 +1,46 @@
 var elements = document.getElementsByTagName('*');
 
+var sourceWordsToTargetWords = [
+    [['policeman', 'policewoman'], 'police officer'],
+    [['policemen', 'policewomen'], 'police officers']
+];
+
+function makeRegex(sourceWords) {
+    return new RegExp('\\b' + sourceWords.join('\\b|\\b') + '\\b', 'g');
+};
+
+function identity(string) {
+    return string;
+};
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+function toUpperCase(string) {
+    return string.toUpperCase();
+};
+
+function makeRegexToTargetWords(sourceWordsToTargetWords, modifyWords) {
+    return sourceWordsToTargetWords.map(function(sourceAndTarget) {
+        var [source,target] = sourceAndTarget;
+        source = source.map(modifyWords);
+        target = modifyWords(target);
+        return [makeRegex(source), target];
+    });
+};
+
+var sourceRegexToTargetWords = makeRegexToTargetWords(sourceWordsToTargetWords, identity).concat(makeRegexToTargetWords(sourceWordsToTargetWords, capitalizeFirstLetter)).concat(makeRegexToTargetWords(sourceWordsToTargetWords, toUpperCase));
+
+function replaceTextWithRegexes(text, sourceRegexToTargetWords) {
+    for (var k = 0; k < sourceRegexToTargetWords.length; k++) {
+        var [regex, targetWord] = sourceRegexToTargetWords[k];
+        var replacedText = text.replace(regex, targetWord);
+        text = replacedText
+    }
+    return text;
+};
+
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -8,9 +49,10 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/cameron/gi, 'yolo');
+            replacedText = replaceTextWithRegexes(text, sourceRegexToTargetWords);
 
             if (replacedText !== text) {
+                console.log('replaced');
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
